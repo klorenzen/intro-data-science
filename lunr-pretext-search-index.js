7e5df95cfe7ac02d36a2d7f13449d50e5e62f0b2
klorenzen <klorenzen@linfield.edu>
2023-08-22 13:10:47 -0700

    Latest build of target web.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,382 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   example.org   https:\/\/example.org   copyright  "
+},
+{
+  "id": "section-1-1",
+  "level": "1",
+  "url": "section-1-1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "What is Data Science?",
+  "body": " What is Data Science?  In the most general terms data science is the process of applying the scientific method to data. In the current information age we live in data is everywhere! We post on social media, wear watches that track our heart beat, take pictures of planets in the universe we never knew existed. We have an unprecedented ability to gather and keep data about our thoughts, our actions, and the universe around us and data science is the study of how to make sense of all this data. At first, this might seem like a daunting task, but data science draws from many other well established fields.  The first field is Mathematics and Statistics. Using statistics we can use the scientific method to draw inference from our data. Hopefully by the end of this class you will be convinced that data science is much more than applied statistics despite how data science draws a lot of inspiration from statistics. Using mathematics we can draw lots of modeling techniques and solutions to tricky issues that arise when dealing with data.  The second field is Computer Science. The data is best accessed through computers and having a solid foundation of working with computers, data types, and programing are certainly helpful! Computer scientists have been studying how to handle large amounts of data for some time. We will draw on programming techniques and ideas of how to process lots of data in an efficient manner from computer science.  Lastly, is an area of expertise! Data comes from somewhere and that context is an important part of how it is used and studied. Data science about behavior on social media will draw ideas and theories from behavioral science. Light patterns gathered from space will draw techniques from astrophysicists. Player performance in a game will draw theories from sports analytics. Not only we will use context to understand where the data is coming from, but these areas will help us ask good and interesting questions about the data.  To be clear, you do not need to be an expert in an area (nor an expert in math, statistics, and computer science) to be a data scientists. But you do need enough of a background that you can make sense of the data and the results. One analogy we can use is that we need to be able to walk before we can run. Knowing enough of these three areas is the ability to walk, and data science allows us run and explore all the data has to offer.   Classification at the Zoo   The local zoo has lots of animals that they house. Each animal is classified into a Kingdom, Genus, Family, and Species (as we remember from a biology class). Some new animals have arrived and we want to classify them based on the data. Below you have the data for 10 known animals and three unknown animals.  Your first task is to use the data to group your known animals together. How do you know you have good groups?  Now, look at the data of the unknown animals. Predict which groups they belong in.  Explain your method of putting the unknown animals into know groups? Which variables or combination of variables did you use?  Brainstorm ways that you could have done this task easier or better? How would computers help?   Congratulations! You just did Data Science! You are a data scientist! Reviewing your first steps as a data scientist, what might be some other areas where a similar model is used?    The first step to doing Data Science is to have data. Data comes in many forms but for the purposes of this section, we will think of data as information stored on a computer. Data is collected about people, countries, natural environment, institutions, and many others. Therefore, most data comes when humans interact with systems that collect data. This could be when you open tiktok on your phone and every like and comment you make. This could be when you swipe your keycard to get into a building. The key insight is that the data collected is reflected of actions of people. We cannot collect data about someone's emotions when they swipe into a building- we can make predictions, we can ask them- but these are not objective pieces of data. -side note about the field of psychology and survey design, but most of the data we will be exploring in this class will deal with objective data-  So, if we only have data about people's reactions to events or other objective, observable facts, what can we tell from data? Well, other objective, observable facts about the **future**. This is the power of data science! It also shapes the types of questions data science can answer. A good rule of thumb is that data science questions should be future looking (What do I expect to happen?), observable (in the future, I can come back and tell if I was right or wrong), and have un-obvious solutions.   Asking Data Science Questions   For each of the following, explain why they are not good data science questions. Then rephrase them to be good data science questions.  Who won the football game last week?  Does my boyfriend love me?  How many members are on an NBA team?   Despite these being silly, remember today when thinking about data science questions so you do not become an example memorialized in a text.    Later this course, when preparing for projects we will work on how to divise good question (and how to answer it).   For each of the ways that gather is collected around you, brainstorm possible data science questions that are future looking, observable, and have un-obvious solutions.  Number of pictures a person posts a day on a social media site.  Number of times people searched for a particular word each day over several years.  The number of webpages written in every language.  Answers to survey results sent out to thousands of people.  Number of times a particular player tries to score and the number of times they are successful.     "
+},
+{
+  "id": "exploration-1",
+  "level": "2",
+  "url": "section-1-1.html#exploration-1",
+  "type": "Exploration",
+  "number": "1.1.1",
+  "title": "Classification at the Zoo.",
+  "body": " Classification at the Zoo   The local zoo has lots of animals that they house. Each animal is classified into a Kingdom, Genus, Family, and Species (as we remember from a biology class). Some new animals have arrived and we want to classify them based on the data. Below you have the data for 10 known animals and three unknown animals.  Your first task is to use the data to group your known animals together. How do you know you have good groups?  Now, look at the data of the unknown animals. Predict which groups they belong in.  Explain your method of putting the unknown animals into know groups? Which variables or combination of variables did you use?  Brainstorm ways that you could have done this task easier or better? How would computers help?   Congratulations! You just did Data Science! You are a data scientist! Reviewing your first steps as a data scientist, what might be some other areas where a similar model is used?   "
+},
+{
+  "id": "exploration-2",
+  "level": "2",
+  "url": "section-1-1.html#exploration-2",
+  "type": "Exploration",
+  "number": "1.1.2",
+  "title": "Asking Data Science Questions.",
+  "body": " Asking Data Science Questions   For each of the following, explain why they are not good data science questions. Then rephrase them to be good data science questions.  Who won the football game last week?  Does my boyfriend love me?  How many members are on an NBA team?   Despite these being silly, remember today when thinking about data science questions so you do not become an example memorialized in a text.   "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "section-1-1.html#exercise-1",
+  "type": "Exercise",
+  "number": "1.1.1",
+  "title": "",
+  "body": "For each of the ways that gather is collected around you, brainstorm possible data science questions that are future looking, observable, and have un-obvious solutions.  Number of pictures a person posts a day on a social media site.  Number of times people searched for a particular word each day over several years.  The number of webpages written in every language.  Answers to survey results sent out to thousands of people.  Number of times a particular player tries to score and the number of times they are successful.   "
+},
+{
+  "id": "section-1-2",
+  "level": "1",
+  "url": "section-1-2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Asking Interesting Questions",
+  "body": " Asking Interesting Questions  "
+},
+{
+  "id": "section-2-1",
+  "level": "1",
+  "url": "section-2-1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Transparency of algorithms",
+  "body": " Transparency of algorithms  "
+},
+{
+  "id": "section-2-2",
+  "level": "1",
+  "url": "section-2-2.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Dangers of scaling",
+  "body": " Dangers of scaling  "
+},
+{
+  "id": "section-2-3",
+  "level": "1",
+  "url": "section-2-3.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Fairness - using data for good.",
+  "body": " Fairness - using data for good.  Check bias that inevitably ends up in algorithms.  "
+},
+{
+  "id": "section-2-4",
+  "level": "1",
+  "url": "section-2-4.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Ethical collection of data and use of personal data.",
+  "body": " Ethical collection of data and use of personal data.  "
+},
+{
+  "id": "section-2-5",
+  "level": "1",
+  "url": "section-2-5.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Accountability",
+  "body": " Accountability  Who is accountable? Consumers, programmers, or government?  "
+},
+{
+  "id": "section-3-1",
+  "level": "1",
+  "url": "section-3-1.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Wrangling",
+  "body": " Wrangling  "
+},
+{
+  "id": "section-3-2",
+  "level": "1",
+  "url": "section-3-2.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Feature engineering",
+  "body": " Feature engineering  "
+},
+{
+  "id": "section-3-3",
+  "level": "1",
+  "url": "section-3-3.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Cleaning",
+  "body": " Cleaning  "
+},
+{
+  "id": "section-4-1",
+  "level": "1",
+  "url": "section-4-1.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Basic sampling techniques",
+  "body": " Basic sampling techniques  "
+},
+{
+  "id": "section-4-2",
+  "level": "1",
+  "url": "section-4-2.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "True, easy to understand, not misleading",
+  "body": " True, easy to understand, not misleading  "
+},
+{
+  "id": "section-4-3",
+  "level": "1",
+  "url": "section-4-3.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Principles of Design",
+  "body": " Principles of Design  Gestalt Principles of Design  "
+},
+{
+  "id": "section-5-1",
+  "level": "1",
+  "url": "section-5-1.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Communicating with self",
+  "body": " Communicating with self  Best practices for notebooks, etc. Notes for self\/team members.  "
+},
+{
+  "id": "section-5-2",
+  "level": "1",
+  "url": "section-5-2.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Communicating with others",
+  "body": " Communicating with others  Like a science fair project: Intro, methods, conclusion.  Tips for STEM written and oral communication.  "
+},
+{
+  "id": "section-6-1",
+  "level": "1",
+  "url": "section-6-1.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Under and over sampling techniques",
+  "body": " Under and over sampling techniques  "
+},
+{
+  "id": "section-6-2",
+  "level": "1",
+  "url": "section-6-2.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Naive Bayes Classifier",
+  "body": " Naive Bayes Classifier  I didn't retype my notes, but here is how you do math. Inline: . As an unlabeled equation: As a labeled equation:   "
+},
+{
+  "id": "section-6-3",
+  "level": "1",
+  "url": "section-6-3.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Linear and Logistic Regression",
+  "body": " Linear and Logistic Regression  "
+},
+{
+  "id": "section-6-4",
+  "level": "1",
+  "url": "section-6-4.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "K-Nearest Neighbors",
+  "body": " K-Nearest Neighbors  "
+},
+{
+  "id": "section-6-5",
+  "level": "1",
+  "url": "section-6-5.html",
+  "type": "Section",
+  "number": "6.5",
+  "title": "Decision Trees",
+  "body": " Decision Trees  "
+},
+{
+  "id": "section-6-6",
+  "level": "1",
+  "url": "section-6-6.html",
+  "type": "Section",
+  "number": "6.6",
+  "title": "K-Means Clustering",
+  "body": " K-Means Clustering  "
+},
+{
+  "id": "section-6-7",
+  "level": "1",
+  "url": "section-6-7.html",
+  "type": "Section",
+  "number": "6.7",
+  "title": "Principal Component Analysis",
+  "body": " Principal Component Analysis  "
+},
+{
+  "id": "section-6-8",
+  "level": "1",
+  "url": "section-6-8.html",
+  "type": "Section",
+  "number": "6.8",
+  "title": "Neural Nets",
+  "body": " Neural Nets  "
+},
+{
+  "id": "chapter-7",
+  "level": "1",
+  "url": "chapter-7.html",
+  "type": "Chapter",
+  "number": "7",
+  "title": "Graph-based Models",
+  "body": " Graph-based Models  "
+},
+{
+  "id": "section-8-1",
+  "level": "1",
+  "url": "section-8-1.html",
+  "type": "Section",
+  "number": "8.1",
+  "title": "How to ask a data science question",
+  "body": " How to ask a data science question  "
+},
+{
+  "id": "section-8-2",
+  "level": "1",
+  "url": "section-8-2.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Data acquisition",
+  "body": " Data acquisition  "
+},
+{
+  "id": "section-8-3",
+  "level": "1",
+  "url": "section-8-3.html",
+  "type": "Section",
+  "number": "8.3",
+  "title": "Choosing your tools",
+  "body": " Choosing your tools  "
+},
+{
+  "id": "section-9-1",
+  "level": "1",
+  "url": "section-9-1.html",
+  "type": "Section",
+  "number": "9.1",
+  "title": "Sports",
+  "body": " Sports  "
+},
+{
+  "id": "section-9-2",
+  "level": "1",
+  "url": "section-9-2.html",
+  "type": "Section",
+  "number": "9.2",
+  "title": "Finance",
+  "body": " Finance  Stock Market Trends    Webscraping  Cleaning Scraped Data  Introduce Time Series Analysis    "
+},
+{
+  "id": "Section-9-2-objectives",
+  "level": "2",
+  "url": "section-9-2.html#Section-9-2-objectives",
+  "type": "Objectives",
+  "number": "9.2",
+  "title": "",
+  "body": "  Webscraping  Cleaning Scraped Data  Introduce Time Series Analysis   "
+},
+{
+  "id": "section-9-3",
+  "level": "1",
+  "url": "section-9-3.html",
+  "type": "Section",
+  "number": "9.3",
+  "title": "Environmental",
+  "body": " Environmental    Expand on PCA  Apply PCA to help identify outliers in large data sets  Apply PCA to aid in feature selection in small data sets    As discussed in , identifying outliers is an important part of data analysis. It is also one of the more dangerous decisions that a data analyst can make. Labeling a unique observation as an outlier can remove valuable information that should be retained from our data set if decided haphazardly or incorrectly. Unfortunately, in high dimensional space, nearly any particular observation might be considered an outlier in at least some feature(s).  On the other hand, in large data sets, it is relatively likely that some outliers will be observed. Or worse, that some experimental error causes an unexpected alteration of the data. Including this information could be just as damaging to our analysis as removing data that should not be considered an outlier. We doing analysis where outliers might be present, recall that a standard practice is to do the analysis twice. Once with the proposed outliers, and once without.  As introduced in *CHAPTER REFERENCE*, PCA provides a way to help us to reduce the dimensionality of a data set. In this way, it can help illuminate us not only to the most important features, but also to data points that are exceptionally far from a typical observation. Unfortunately, PCA depends on variance to make its determination, and outliers can have a large impact on the variance of a feature causing the outliers to be harder to distinguish.  For this reason, we introduce Robust PCA. Our idea is that by taking many subsamples of our data, we expect not to be heavily impacted by what we expect to be only a few outliers. The question that must be answered now is how do we determine which subset to use as our ``typical'' data. Our intuition for outliers in PCA is that they would be far away from most of the data in the reduced space provided by PCA. One way to think about the size of the space that PCA finds is through the determinant. A small determinant would correspond to a compact space in which the data lies. With this intuition, we may hazard to guess that a good set to run PCA on would provide compact space. Our goal is to sample from our data set to find a minimum determinant of the covariance matrix.  When we subsample, there are a few decisions that we must make. One of the most important decisions in such a process is the size of the subsets chosen. In PCA we still want a representative sample of the entire data, so we should not choose a small (under 50%) proportion of our data. On the other hand, if the sampled set is too large, we are likely to choose outliers. A general rule of thumb is to select about 75% of the entire data set for each sample. Note that one cost of a resampling method such as this is cost. Instead of running PCA once, we will be running it potentially hundreds or thousands of times. As such, data sets with more than a few features might be time consuming and will eventually be impractical to check.   Minimum Covariance Determinant  We will now apply the minimum covariance determinant method to a dataset to identify potential outliers. Fortunately for us, this has already been implemented in the SciKit-Learn!    "
+},
+{
+  "id": "Section-9-3-objectives",
+  "level": "2",
+  "url": "section-9-3.html#Section-9-3-objectives",
+  "type": "Objectives",
+  "number": "9.3",
+  "title": "",
+  "body": "  Expand on PCA  Apply PCA to help identify outliers in large data sets  Apply PCA to aid in feature selection in small data sets   "
+},
+{
+  "id": "section-9-4",
+  "level": "1",
+  "url": "section-9-4.html",
+  "type": "Section",
+  "number": "9.4",
+  "title": "Marketing",
+  "body": " Marketing  "
+},
+{
+  "id": "section-9-5",
+  "level": "1",
+  "url": "section-9-5.html",
+  "type": "Section",
+  "number": "9.5",
+  "title": "Social networks",
+  "body": " Social networks  "
+},
+{
+  "id": "section-9-6",
+  "level": "1",
+  "url": "section-9-6.html",
+  "type": "Section",
+  "number": "9.6",
+  "title": "Psychology",
+  "body": " Psychology  "
+},
+{
+  "id": "section-9-7",
+  "level": "1",
+  "url": "section-9-7.html",
+  "type": "Section",
+  "number": "9.7",
+  "title": "Health sciences",
+  "body": " Health sciences  Microbiology, possibly.  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
